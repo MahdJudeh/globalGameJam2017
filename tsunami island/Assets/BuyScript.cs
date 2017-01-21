f873727bbff3a72c0a3c11f5935873c598866642
tserpas1289 <tserpas1289@gmail.com>
2017-01-21 01:41:00 -0600

    some stuff i forgot

---
+++ tsunami island/Assets/BuyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyScript : MonoBehaviour {
+
+    public Text text;
+	
+	void OnTriggerStay()
+    {
+        
+        text.enabled=true;
+         
+	}
+
+    void OnTriggerExit()
+    {
+        //text.enabled = false;
+    }
+}
