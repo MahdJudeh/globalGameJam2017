1caf99dd7941bbeaa78dce82d2acac3f96d0f773
MahdJudeh <mjudeh1@lsu.edu>
2017-01-22 04:09:46 -0600

    added waveSpawner again

--- tsunami island/Assets/WaveSpawner.cs
+++ tsunami island/Assets/WaveSpawner.cs
@@ -8,16 +8,33 @@ public class WaveSpawner : MonoBehaviour {
     private int divider;
 	// Use this for initialization
 	void Start () {
-        time = 0;
+        time = 1;
         divider = 1;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        
+        int rand = Random.Range(1, 4);
+        if ((time) % ((Mathf.Floor(5000 / divider))) == 0)
         {
-            Instantiate(Wave, new Vector3(Random.Range(150.0f, 200.0f), 0, Random.Range(150.0f, 200.0f)), Quaternion.identity);
+            if (rand == 1)
+            {
+                Instantiate(Wave, new Vector3(Random.Range(150.0f, 200.0f), 0, 0), Quaternion.Euler(0, 180, 0));
+            }
+            else if (rand == 2)
+            {
+                Instantiate(Wave, new Vector3(0, 0, Random.Range(150.0f, 200.0f)), Quaternion.Euler(0, 90, 0));
+            }
+            else if (rand == 3)
+            {
+                Instantiate(Wave, new Vector3(Random.Range(-150.0f, -200.0f), 0, 0), Quaternion.Euler(0, 0, 0));
+            }
+            else if (rand == 4)
+            {
+                Instantiate(Wave, new Vector3(0, 0, Random.Range(-150.0f, -200.0f)), Quaternion.Euler(0, -90, 0));
+            }
         }
+        time += 2;
        
     }
 }
