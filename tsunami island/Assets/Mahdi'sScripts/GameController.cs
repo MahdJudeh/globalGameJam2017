b527badeb64e83bea4bbd6b25a4d1ed776dacafa
MahdJudeh <mjudeh1@lsu.edu>
2017-01-22 01:49:40 -0600

    Added wave script basics

--- tsunami island/Assets/Mahdi'sScripts/GameController.cs
+++ tsunami island/Assets/Mahdi'sScripts/GameController.cs
@@ -8,10 +8,12 @@ public class GameController : MonoBehaviour {
     public GameObject Moon;
     public GameObject Sun;
     public GameObject Ocean;
+    public GameObject Wave;
     public Text dayText;
     private int day;
     private int time;
     private float rotation = .003f;
+    private int max;
 
 	// Use this for initialization
 	void Start () {
@@ -21,12 +23,15 @@ public class GameController : MonoBehaviour {
         Sun.transform.position = new Vector3(50f, 0f, 0f);
         Sun.transform.rotation = new Quaternion(0f, -90f, 0f, 0f);
         dayText.text = dayText.text + " " + day;
+        max = 1;
+
 
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(time < 11400)
+        int rand = Random.Range(0, max);
+        if (time < 11400)
         {
             Ocean.transform.localScale += new Vector3(0f, 0.001f, 0f);
             Sun.transform.Rotate(rotation, 0f, 0f);
@@ -38,6 +43,10 @@ public class GameController : MonoBehaviour {
                 Sun.transform.position = new Vector3(50f, 0f, 0f);
                 Sun.transform.rotation = new Quaternion(0f, -90f, 0f, 0f);
             }
+            if(time%1000 == 0 && rand == 1)
+            {
+                Instantiate(Wave, new Vector3(Random.Range(250.0f, 500.0f), 0, Random.Range(250.0f, 500.0f)), Quaternion.identity);
+            }
         }
         else if(time < 22800)
         {
@@ -47,6 +56,7 @@ public class GameController : MonoBehaviour {
             if(time == 22800)
             {
                 Moon.GetComponent<Light>().enabled = false;
+                time = 0;
             }
         }
 	}
