9bb00a8c0bcfba18bf78dc2d831699e6117bd41a
MahdJudeh <mjudeh1@lsu.edu>
2017-01-22 03:29:22 -0600

    added waveSpawner again

---
+++ tsunami island/Assets/WaveSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+    public GameObject Wave;
+    private int time;
+    private int divider;
+	// Use this for initialization
+	void Start () {
+        time = 0;
+        divider = 1;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        {
+            Instantiate(Wave, new Vector3(Random.Range(150.0f, 200.0f), 0, Random.Range(150.0f, 200.0f)), Quaternion.identity);
+        }
+       
+    }
+}
